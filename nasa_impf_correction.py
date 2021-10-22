5a5c92be6b0aff092c3f7e83ac8f990a0e056dc5
Daniel Philipp <daniel@spotter4you.de>
2021-10-22 12:55:09 +0000

    Bug fix

--- nasa_impf_correction.py
+++ nasa_impf_correction.py
@@ -2,11 +2,11 @@ import numpy as np
 
 PARAMETERS = {'MSG1': {'SLOPE': {'ch1': 0.85628,'ch2': 0.91591,'ch3': 1.05427},
                        'INTERCEPT': {'ch1': 0.0, 'ch2': 0.0, 'ch3': 0.0}},
-             {'MSG2': {'SLOPE': {'ch1': 0.91689,'ch2': 0.93875,'ch3': 1.04209},
+             'MSG2': {'SLOPE': {'ch1': 0.91689,'ch2': 0.93875,'ch3': 1.04209},
                        'INTERCEPT': {'ch1': 0.0, 'ch2': 0.0, 'ch3': 0.0}},
-             {'MSG3': {'SLOPE': {'ch1': 0.89353,'ch2': 0.95154,'ch3': 1.03764},
+             'MSG3': {'SLOPE': {'ch1': 0.89353,'ch2': 0.95154,'ch3': 1.03764},
                        'INTERCEPT': {'ch1': 0.0, 'ch2': 0.0, 'ch3': 0.0}},
-             {'MSG4': {'SLOPE': {'ch1': 0.90091,'ch2': 0.97954,'ch3': 1.06372},
+             'MSG4': {'SLOPE': {'ch1': 0.90091,'ch2': 0.97954,'ch3': 1.06372},
                        'INTERCEPT': {'ch1': 0.0, 'ch2': 0.0, 'ch3': 0.0}}
              }
 
@@ -20,6 +20,9 @@ def correct_nasa_impf(vis006, vis008, nir016, msg_index):
               
     msg = 'MSG{:d}'.format(msg_index)
     params = PARAMETERS[msg]
+
+    print('NASA to IMPF: ', msg)
+    print('NASA to IMPF slope ch1: ', params['SLOPE']['ch1'])
 
     vis006 = _apply_correction(vis006, 
                                params['SLOPE']['ch1'],
